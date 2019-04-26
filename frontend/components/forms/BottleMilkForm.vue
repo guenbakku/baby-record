d16a53330e30f93eb8f99eaa162701e79a56365f
Nguyen Van Bach <vanbach1213@gmail.com>
2019-04-26 19:21:36 +0900

    Implement speed dial

--- frontend/components/forms/BottleMilkForm.vue
+++ frontend/components/forms/BottleMilkForm.vue
@@ -1,3 +1,42 @@
 <template>
-  <div>BottleMilkForm</div>
+  <form>
+    <v-text-field
+      v-model="form.started"
+      label="Bắt đầu"
+      required
+    ></v-text-field>
+    <v-text-field
+      v-model="form.duration"
+      label="Thời gian"
+      mask="##:##:##"
+      placeholder="00:00:00"
+      required
+    ></v-text-field>
+    <v-text-field
+      v-model="form.breast_volume"
+      label="Lượng sữa mẹ"
+      suffix="ml"
+    ></v-text-field>
+    <v-text-field
+      v-model="form.fomular_volume"
+      label="Lượng sữa công thức"
+      suffix="ml"
+    ></v-text-field>
+    <v-text-field
+      v-model="form.memo"
+      label="Ghi chú"
+    ></v-text-field>
+    {{ form.duration }}
+  </form>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    form: {
+      started: null,
+      duration: null
+    }
+  })
+}
+</script>
