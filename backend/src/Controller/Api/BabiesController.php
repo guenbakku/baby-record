e009b22e000dda1eb09169c6e1502b7cf2c5cab2
Nguyen Van Bach <vanbach1213@gmail.com>
2019-09-07 22:21:20 +0900

    Auto add `user_id` when add or edit baby

--- backend/src/Controller/Api/BabiesController.php
+++ backend/src/Controller/Api/BabiesController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller\Api;
 
+use Cake\Event\Event;
+
 /**
  * Babies Controller
  *
@@ -8,5 +10,27 @@ namespace App\Controller\Api;
  */
 class BabiesController extends AppController
 {
+    public function add() {
+        $this->Crud->on('beforeSave', function (Event $event) {
+            $entity = $event->getSubject()->entity;
+            if (empty($entity->getErrors())) {
+                $entity->user_id = $this->Auth->user('id');
+            }
+        });
+
+        return $this->Crud->execute();
+    }
+
+    public function edit() {
+        $this->Crud->on('beforeSave', function (Event $event) {
+            $entity = $event->getSubject()->entity;
+            if (empty($entity->getErrors())) {
+                $entity->user_id = $this->Auth->user('id');
+            }
+        });
+
+        return $this->Crud->execute();
+    }
+
     // Inheriting the rest of FriendOfCake/Crud's methods.
 }
